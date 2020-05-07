c4c5837a65be847906394254fa5800116bfa8ee5
amonrs <amonsthapit@live.com>
2020-05-06 19:18:19 -0700

    asfgkshdfjkgh

---
+++ battleship.py
@@ -0,0 +1,47 @@
+# battleship ai
+
+board = []
+
+ships = [2,3,3,4,5]
+
+
+
+# for several times:
+    # randomly place the ships on the board
+    # run an algorithm to solve it
+    # get results (number to moves to win)
+
+# OR
+
+# tournament style
+    # ...
+
+
+
+
+
+
+
+
+# random
+
+# hunt algorithm
+    # explore nearby after hit
+
+# probability density function / heatmap
+    # https://cliambrown.com/battleship/methodology.php#options
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+# https://paulvanderlaken.com/2019/01/21/beating-battleships-with-algorithms-and-ai/
