2d6e8803bea65d0e3883135978a7deb5c64878db
amonrs <amonsthapit@live.com>
2020-05-14 22:13:01 -0700

    working on tournament

--- battleship.py
+++ battleship.py
@@ -16,11 +16,6 @@ import rl
 
 
 
-
-
-
-
-
 def plot_results(random_moves=[], hunt_moves=[], prob_moves=[], rl_moves=[]):
 
 
@@ -34,12 +29,6 @@ def plot_results(random_moves=[], hunt_moves=[], prob_moves=[], rl_moves=[]):
     plt.ylabel('no. of moves to win')
     
     plt.show()
-
-
-
-
-
-
 
 
 
@@ -59,24 +48,60 @@ def play_solo(no_of_plays=1):
         prob_moves.append( probability_density.play( copy.deepcopy(board) ) )
     rl_moves = rl.play(no_of_plays)
 
-    print ('average moves for each algo to win: ')
-    print('random: ', statistics.mean(random_moves), '---', random_moves)
-    print('hunt: ', statistics.mean(hunt_moves), '---', hunt_moves)
-    print('prob: ', statistics.mean(prob_moves), '---', prob_moves)
-    print('RL: ', statistics.mean(rl_moves), '---', rl_moves)
+    print ( 'average moves for each algo to win: ' )
+    print( 'random: ', statistics.mean(random_moves), '---', random_moves )
+    print( 'hunt: ', statistics.mean(hunt_moves), '---', hunt_moves )
+    print( 'prob: ', statistics.mean(prob_moves), '---', prob_moves )
+    print( 'RL: ', statistics.mean(rl_moves), '---', rl_moves )
 
-    plot_results(random_moves, hunt_moves, prob_moves, rl_moves)
+    plot_results( random_moves, hunt_moves, prob_moves, rl_moves )
 
-def play_tournament():
-    # lets not do this either :(
-    pass
+
+
+def play_tournament(no_of_plays=1):
+    # 6 DIFFERENT GAME COMPETITIONS
+
+    # random vs. hunt
+    random_vs_hunt = { 'random': 0, 'hunt': 0 }
+    for i in range(no_of_plays):
+        random_moves = random_algo.play( utils.get_board('random_ships') )
+        hunt_moves = hunt_algo.play( utils.get_board('random_ships') )
+        if random_moves < hunt_moves:
+            random_vs_hunt['random'] += 1
+        elif hunt_moves < random_moves:
+            random_vs_hunt['hunt'] += 1
+
+    # random vs. prob
+    random_vs_prob = { 'random': 0, 'prob': 0 }
+    for i in range(no_of_plays):
+        random_moves = random_algo.play( utils.get_board('random_ships') )
+        prob_moves = probability_density.play( utils.get_board('random_ships') )
+        if random_moves < prob_moves:
+            random_vs_prob['random'] += 1
+        elif prob_moves < random_moves:
+            random_vs_prob['prob'] += 1
+    
+    # hunt vs. prob
+    hunt_vs_prob = { 'hunt': 0, 'prob': 0 }
+    for i in range(no_of_plays):
+        hunt_moves = hunt_algo.play( utils.get_board('random_ships') )
+        prob_moves = probability_density.play( utils.get_board('random_ships') )
+        if hunt_moves < prob_moves:
+            hunt_vs_prob['hunt'] += 1
+        elif prob_moves < hunt_moves:
+            hunt_vs_prob['prob'] += 1
+
+    # random vs. RL
+
+    # hunt vs. RL
+
+    # prob vs. RL
+
+
 
 def play_ai_vs_player():
     # lets not do this one :(
     pass
-
-
-
 
 
 
@@ -95,7 +120,6 @@ def main():
     play_solo(30)
     # test_rl()
     # test_prob()
-
 
 if __name__ == '__main__':
     main()
